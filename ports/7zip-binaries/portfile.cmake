a168e0ac4d307c86e95b6ea5a28be5b3405fd74b
dhanush.murali <hp.dhanushm@gmail.com>
2022-02-21 18:43:36 +0530

    removing sha512

--- ports/7zip-binaries/portfile.cmake
+++ ports/7zip-binaries/portfile.cmake
@@ -1,7 +1,6 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pythagoras9/7zipBinaries
-	SHA512 a6491b2680979feb2f09e7ffb4f3c939b75057b6b4b90136b6cb72eaf5a462d3079dfb5836744e53bf4946bd575d4449f87129d6e50045732d7fba11c2be57dd
     HEAD_REF main
 )
 
